d7b33bed6a1b98728b982f13390474a24fa79bec
LOB79 <84478376+LOB79@users.noreply.github.com>
2021-06-02 19:34:00 -0300

    Update app.js
    Formatacao do JS para organizacao da tabela

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,57 @@
 function lelogin (){
-    let strlogin = localStorage.getItem('bd');
+    let strlogin = localStorage.getItem('db');
     let objsite = {};
 
     if (strlogin) {
         objsite = JSON.parse (strlogin);
     }
     else {
-        objsite = { logins: [
-                         {site:"Google", user:"Leo", senha:"leo42"},
-                         {site:"Googe", user:"Le", senha:"leo2"}
-            ]}
+        objsite = { 
+            logins: [
+                         {site:"Google", user:"Leo", senha:"7Kl01"},
+                         {site:"Amazon", user:"Leonardo", senha:"09Le0"}
+            ]
+        }
 
         }
         return objsite; 
 }
 
 function salvalogin (login) {
-    localStorage.setItem ('bd' , JSON.stringify (login));
+    localStorage.setItem ('db' , JSON.stringify (login));
 }
 function imprimirlogin (){
     let tela = document.getElementById('tela');
-    let strHtml = '';
+    let tela1 = document.getElementById('tela1');
+    let tela2 = document.getElementById('tela2');
+    let strHtml1 = '';
+    let strHtml2 = '';
+    let strHtml3 = '';
     let objsite = lelogin ();
 
-    for (i=0; i< objsite.logins.length; i++){
-        strHtml = strHtml +  `<p> ${objsite.logins[i].site} - ${objsite.logins[i].user} - ${objsite.logins[i].senha} </p>`
+    for (i=0; i < objsite.logins.length; i++){
+        strHtml1 = strHtml1 +  `<p> ${objsite.logins[i].site} </p>`
+        strHtml2 = strHtml2 +  `<p> ${objsite.logins[i].user} </p>`
+        strHtml3 = strHtml3 +  `<p> ${objsite.logins[i].senha} </p>`
     }
-    tela.innerHTML = strHtml;
+    tela.innerHTML = strHtml1;
+    tela1.innerHTML = strHtml2;
+    tela2.innerHTML = strHtml3;
 }
-
 function incluirlogin (){
+    let objsite = lelogin();
     let nsite = document.getElementById("bdsite").value;
     let nuser = document.getElementById("bduser").value;
     let nsenha = document.getElementById("bdsenha").value;
     let novasenha = {
         site: nsite,
-        user:nuser,
-        senha:nsenha
+        user: nuser,
+        senha: nsenha
     };
-    objsite.login.push (novasenha);
+    objsite.logins.push (novasenha);
     salvalogin (objsite);
 
-    
-
+ imprimirlogin();   
 }
-document.getElementById ('bdbutton').addEventListener ('click', imprimirlogin, salvalogin);
+document.getElementById ('bdbsalvar').addEventListener ('click', incluirlogin);
+document.getElementById ('bdbutton').addEventListener ('click', imprimirlogin);
